cb3de96cd974fbbc1e466d9dd8fcd8f50c50879c
Andrew <a129924@gmail.com>
2023-02-22 18:35:37 +0800

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
 
-const labels = [
-  { labelname: "長度", id: 1 },
-  { labelname: "組數", id: 2 },
-  { labelname: "相似度", id: 3 },
+const labelSettings = [
+  { labelname: "長度", id: 1 , hasCheckBox : false},
+  { labelname: "組數", id: 2, hasCheckBox: false},
+  { labelname: "相似度", id: 3, hasCheckBox: true },
 ]
 
 
@@ -15,21 +15,44 @@ function text_align(align) {
 function App() {
 
 
-  const labelList = labels.map(label =>
-    <div className="labelitem"
-      key={label.id}
-    >
-      <label>{label.labelname}</label>
-      <input
-        type="text" />
-      <input
-        type="checkbox"
-        name="check"
-        defaultChecked={true}
-      />啟用
+  // const labelList = labelSettings.map(labelSetting =>
+  //   <div className="labelitem"
+  //     key={labelSetting.id}
+  //   >
+  //     <label>{labelSetting.labelname}</label>
+  //     <input type="text" />
+  //     <input type="checkbox" name="check" defaultChecked={true}/>啟用
 
-    </div>
-  )
+
+
+  //   </div>
+  // )
+  const labelList = labelSettings.map(labelSetting => {
+    const shouldRenderCheckbox = labelSetting.hasCheckBox;
+    return (
+      <div className="labelitem" key={labelSetting.id}>
+        
+        <div className="label">
+          <label>{labelSetting.labelname}</label>
+        </div>
+
+        <div className="textbox">
+          <input type="text" />
+        </div>
+
+        <div className="checkbox">
+          {shouldRenderCheckbox ? (
+            <input type="checkbox" name="check" defaultChecked={true} />
+          ):(
+              <div className="ABC">
+                <button></button>
+              </div>
+          )}
+          <span>{shouldRenderCheckbox ? '啟用' : ""}</span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div className="App">
